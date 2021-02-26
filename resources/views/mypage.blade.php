8897190dac8fd8bcf728b25e17fba80db4bd00c2
Masashige-T <m.t0719@outlook.jp>
2021-02-26 15:10:04 +0900

    update: マイページ完成

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -5,37 +5,71 @@
 @section('lastname',$lastname)
 
 @section('contents')
-	<h1>マイページ</h1>
-	いらっしゃいませ{{$lastname}}様
-	<div>
-		会員情報の変更は下部のボタンからお進みください<br>
-		<a href="{{url('editInfo')}}">会員情報を変更</a>
-	</div>
-	<h1>お客さんの注文履歴</h1>
-	<!--注文時間フィルター-->
-	<form method="post" action="{{url('myPage')}}">
-		{{csrf_field()}}
-		<select name="date_filter">
-			<option value="-6"
-				@if($dateFilter==-6)
-					{{'selected="selected"'}}
-				@endif
-			>過去六ヶ月</option>
-			<option value="-12"
-				@if($dateFilter==-12)
-					{{'selected="selected"'}}
-				@endif			
-			>過去一年間</option>
-			<option value="-24"
-				@if($dateFilter==-24)
-					{{'selected="selected"'}}
-				@endif			
-			>過去二年間</option>
-		</select>
-		<input type="submit" value="検索">
-	</form>
+	<div class="Mypage">
+    <h1 class="Contents-title">マイページ</h1>
+    <p class="Mypage__welcome">いらっしゃいませ、{{$lastname}}様</p>
+    <div class="User-data-change">
+      <p>会員情報の変更は下部のボタンからお進みください</p>
+      <a class="Button Button--red" href="{{url('editInfo')}}">会員情報を変更</a>
+    </div>
+    <div class="Order-history Mypage__order-history">
+      <h1 class="Order-history__title">お客様の注文履歴</h1>
+      <!--注文時間フィルター-->
+      <form method="post" action="{{url('myPage')}}">
+        {{csrf_field()}}
+        <select class="Order-history__select" name="date_filter">
+          <option value="-6"
+          @if($dateFilter==-6)
+          {{'selected="selected"'}}
+          @endif
+          >過去六ヶ月</option>
+          <option value="-12"
+          @if($dateFilter==-12)
+          {{'selected="selected"'}}
+          @endif>過去一年間</option>
+          <option value="-24"
+          @if($dateFilter==-24)
+          {{'selected="selected"'}}
+          @endif>過去二年間</option>
+        </select>
+        <input class="Button Button--red Order-history__button" type="submit" value="検索">
+      </form>
+      <div class="Order-history__labels">
+        <span class="Order-history__label"></span>
+        <span class="Order-history__label">商品名</span>
+        <span class="Order-history__label">注文日</span>
+        <span class="Order-history__label">数量</span>
+        <span class="Order-history__label">金額</span>
+        <span class="Order-history__label">注文状況</span>
+      </div>
+      <div class="Order-history__list">
+        @foreach($orderInfos as $key=>$value)
+        <div class="Order-history__img">
+          <a href="{{url('proDetails',['products_id'=>$value->id])}}">
+            <img src="{{asset($value->previewfile)}}">
+          </a>
+        </div>
+        <div class="Order-history__text">
+          <a href="{{url('proDetails',['products_id'=>$value->id])}}">{{$value->name}}</a>
+        </div>
+        <div class="Order-history__text">
+          {{substr($order[$key]->created_at,0,10)}}
+        </div>
+        <div class="Order-history__text">
+          {{$order[$key]->quantity}}
+        </div>
+        <div class="Order-history__text">
+          &yen;{{$value->price}}
+        </div>
+        <div class="Order-history__text">
+          {{$state[$key]}}
+        </div>
+        @endforeach
+      </div>
+    </div>
+  </div>
 	<!--注文履歴-->
-	<table>
+	{{-- <table>
 		<tr>
 			<td></td>
 			<td>商品名</td>
@@ -72,6 +106,6 @@
 		</td>
 		</tr>
 		@endforeach
-	</table>
+	</table> --}}
 
 @endsection
